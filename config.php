1ca8e0161af752c562c39c5688091ff5bfbb02ba
Iceberg1109 <smartdev1109@outlook.com>
2019-12-18 04:17:50 +0800

    init a project

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "amerifir_onst";
+// MySQL Database Password
+$sql_db_pass = "usman123";
+// MySQL Database Name
+$sql_db_name = "amerifir_onstaller";
+
+// Site URL
+$site_url = "https://amerifirst.online/onstellar"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "a5d6a436-deac-432c-bc8c-777caad9b6e4"; // Your purchase code, don't give it to anyone. 
+?>
